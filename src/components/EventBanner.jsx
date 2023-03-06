256f8498888c13d8cd374ebd21e1200d4a37d984
kittty0520 <dheud1004@gmail.com>
2023-03-06 18:17:43 +0900

    Change Detail of Images

--- src/components/EventBanner.jsx
+++ src/components/EventBanner.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function EventBanner() {
 	return (
-		<div className='w-full h-60 my-4 bg-gray-100 relative overflow-hidden'>
-			<img src='image/banner.jpg' alt='eventBanner' />
+		<div className='w-full h-60 my-4 bg-orange-300 relative overflow-hidden'>
+			<img src='../image/banner.jpg' alt='eventBanner' className='opacity-80' />
 			<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center'>
 				<h3 className='text-2xl font-semibold mb-4'>NEW COLLECTION ARRIVED</h3>
 				<p>따뜻한 봄을 맞이하는 23SS 신상</p>
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { FaUser } from 'react-icons/fa';
 export default function User({ user: { photoURL, displayName } }) {
 	return (
 		<div className='flex items-center shrink-0'>
-			<img
+			{/* <img
 				src={photoURL}
 				alt={displayName}
 				className='w-10 h-10 mr-2 rounded-full'
-			/>
-			<span className='hidden md:block'>{displayName}</span>
+			/> */}
+			<FaUser className='text-2xl' />
+			{/* <span className='hidden md:block'>{displayName}</span> */}
 		</div>
 	);
 }
