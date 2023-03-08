46453828c1da0515a3f0bf416c2c6a41114055a3
kittty0520 <dheud1004@gmail.com>
2023-03-08 17:18:21 +0900

    Change Slides component CSS

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Slide({ title, imgPath, photographer }) {
 	return (
 		<div className='w-full h-96 overflow-hidden flex items-center justify-between relative'>
-			<img src={`image/${imgPath}`} alt='banner03' className='w-1/2 lg:w-1/4' />
-			<div className='w-1/2 lg:w-72 flex-shrink-0 flex flex-col  items-center'>
+			<img
+				src={`image/${imgPath}`}
+				alt='banner03'
+				className='w-full sm:w-1/2 lg:w-1/4'
+			/>
+			<div className='absolute w-full sm:static sm:w-1/2 lg:w-72 flex-shrink-0 flex flex-col  items-center'>
 				<p className='text-3xl text-white font-semibold mb-4'>{title}</p>
 				<button className='px-4 py-2 border border-gray-50 text-white'>
 					바로가기
